060ca1e4fd884775e9800d3420d89b23070304bb
mumin-ahmod <mumin.ahmed.ledp@gmail.com>
2023-09-11 17:22:32 +0600

    1st commit

---
+++ index.php
@@ -0,0 +1,41 @@
+
+<?php include 'includes/header.php' ?>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+    <?php include 'includes/sidebar.php' ?>
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+            <?php include 'includes/navbar.php' ?>
+
+                <h2> Welcome to EduHub </h2>
+
+
+            </div>
+            <!-- End of Main Content -->
+
+       <?php include 'includes/footer.php' ?>
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <?php include 'includes/script.php' ?>
+</body>
+
+</html>
